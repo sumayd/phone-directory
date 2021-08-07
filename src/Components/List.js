5651e7e4914217cf211f93acadf7848d45eb54a6
Sümeyye <sumeyyeaydemir@gmail.com>
2021-08-07 18:07:41 +0300

    add filter function

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
+
 import './List.css';
 
 class List extends Component {
-    static propTypes ={
-        contacts: PropTypes.array.isRequired,
+    state = {
+        filterText: ''
+    }; 
+
+    onChangeFilterText = (e) =>{
+        this.setState({
+            filterText: e.target.value
+        })
     };
     render() {
+        const filteredContacts = this.props.contacts.filter(
+            contact =>{
+                return contact.name.toLowerCase().indexOf(
+                    this.state.filterText.toLowerCase()
+                ) !== -1
+            }
+        );
         return (
-            <div className={"lisContent"}>
+            <div className={"listContent"}>
                 <h1>Phone Directory</h1>
                 <div className={"personFiltre"}>
-                    <input name={"filter"} id={"filter"} placeholder={"Search"}/>
+                    <input 
+                    value={this.state.filterText}
+                    onChange={this.onChangeFilterText}
+                    name={"filter"} 
+                    id={"filter"} 
+                    placeholder={"Search"}/>
+
                     <ul className={"personList"}>
-                        {this.props.contacts.map( contact =>
+                        {filteredContacts.map( contact =>
                                 <li key={contact.phone}>
                                     <span className={"name"}>{contact.name}</span>
                                     <span className={"phone"}>{contact.phone}</span>
